19293eaaa51d238a39b16de9d1ddce77ebf97569
josephMG <joseph780116.mg@gmail.com>
2019-09-11 23:48:59 +0800

    [Day 4] Google Vision AI - 3

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 // Sample vision-quickstart uses the Google Cloud Vision API to label an image.
 package main
 
-import vision "./modules/vision"
+import (
+	"os"
+
+	vision "./modules/vision"
+)
 
 func main() {
-	vision.DetectLabel()
+	// vision.DetectLabel(os.Stdout, "./testdata/furniture.jpg")
+	//vision.DetectText(os.Stdout, "./testdata/las-vegas.jpg")
+	vision.DetectFaces(os.Stdout, "./testdata/face.jpg")
 }
--- modules/vision/vision.go
+++ modules/vision/vision.go
@@ -3,34 +3,37 @@ package vision
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 	"os"
 
 	vision "cloud.google.com/go/vision/apiv1"
 )
 
-func DetectLabel() {
+func DetectLabel(w io.Writer, file string) error {
 	ctx := context.Background()
 
 	// Creates a client.
 	client, err := vision.NewImageAnnotatorClient(ctx)
 	if err != nil {
 		log.Fatalf("Failed to create client: %v", err)
+		return err
 	}
 	defer client.Close()
 
 	// Sets the name of the image file to annotate.
-	filename := "./testdata/furniture.jpg"
 
-	file, err := os.Open(filename)
+	f, err := os.Open(file)
 	if err != nil {
 		log.Fatalf("Failed to read file: %v", err)
+		return err
 	}
-	defer file.Close()
-	image, err := vision.NewImageFromReader(file)
+	image, err := vision.NewImageFromReader(f)
 	if err != nil {
 		log.Fatalf("Failed to create image: %v", err)
+		return err
 	}
+	defer f.Close()
 
 	labels, err := client.DetectLabels(ctx, image, nil, 10)
 	if err != nil {
@@ -41,4 +44,77 @@ func DetectLabel() {
 	for _, label := range labels {
 		fmt.Println(label.Description)
 	}
+	return nil
+}
+
+func DetectText(w io.Writer, file string) error {
+	ctx := context.Background()
+
+	client, err := vision.NewImageAnnotatorClient(ctx)
+	if err != nil {
+		return err
+	}
+
+	f, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	image, err := vision.NewImageFromReader(f)
+	if err != nil {
+		return err
+	}
+	annotations, err := client.DetectTexts(ctx, image, nil, 10)
+	if err != nil {
+		return err
+	}
+
+	if len(annotations) == 0 {
+		fmt.Fprintln(w, "No text found.")
+	} else {
+		fmt.Fprintln(w, "Text:")
+		for _, annotation := range annotations {
+			fmt.Fprintf(w, "%q\n", annotation.Description)
+		}
+	}
+
+	return nil
+}
+
+func DetectFaces(w io.Writer, file string) error {
+	ctx := context.Background()
+
+	client, err := vision.NewImageAnnotatorClient(ctx)
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	f, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	image, err := vision.NewImageFromReader(f)
+	if err != nil {
+		return err
+	}
+	annotations, err := client.DetectFaces(ctx, image, nil, 10)
+	if err != nil {
+		return err
+	}
+	if len(annotations) == 0 {
+		fmt.Fprintln(w, "No faces found.")
+	} else {
+		fmt.Fprintln(w, "Faces:")
+		for i, annotation := range annotations {
+			fmt.Fprintln(w, "  Face", i)
+			fmt.Fprintln(w, "    Anger:", annotation.AngerLikelihood)
+			fmt.Fprintln(w, "    Joy:", annotation.JoyLikelihood)
+			fmt.Fprintln(w, "    Surprise:", annotation.SurpriseLikelihood)
+		}
+	}
+	return nil
 }
