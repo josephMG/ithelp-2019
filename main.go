bf006b4e56372bf8712d0d1469b05710a08dfabe
josephMG <joseph780116.mg@gmail.com>
2019-09-15 23:55:53 +0800

    [Day 8] Google Video Intelligence AI - 3

--- main.go
+++ main.go
@@ -10,7 +10,9 @@ import (
 
 func main() {
 	// vision.DetectLabel(os.Stdout, "./testdata/furniture.jpg")
-	//vision.DetectText(os.Stdout, "./testdata/las-vegas.jpg")
+	// vision.DetectText(os.Stdout, "./testdata/las-vegas.jpg")
 	// vision.DetectFaces(os.Stdout, "./testdata/face.jpg")
-	video.DemoCode(os.Stdout, "gs://cloud-samples-data/video/cat.mp4")
+	// video.DemoCode(os.Stdout, "gs://cloud-samples-data/video/cat.mp4")
+	video.ShotChangeURI(os.Stdout, "gs://cloud-samples-data/video/gbikes_dinosaur.mp4")
+	video.TextDetectionGCS(os.Stdout, "gs://python-docs-samples-tests/video/googlework_short.mp4")
 }
--- modules/video/video.go
+++ modules/video/video.go
@@ -58,3 +58,92 @@ func DemoCode(w io.Writer, file string) error {
 	}
 	return nil
 }
+
+func ShotChangeURI(w io.Writer, file string) error {
+	ctx := context.Background()
+	client, err := video.NewClient(ctx)
+	if err != nil {
+		return err
+	}
+
+	op, err := client.AnnotateVideo(ctx, &videopb.AnnotateVideoRequest{
+		Features: []videopb.Feature{
+			videopb.Feature_SHOT_CHANGE_DETECTION,
+		},
+		InputUri: file,
+	})
+	if err != nil {
+		return err
+	}
+
+	fmt.Fprintf(w, "Running....\n")
+	resp, err := op.Wait(ctx)
+	if err != nil {
+		return err
+	}
+
+	// A single video was processed. Get the first result.
+	result := resp.AnnotationResults[0].ShotAnnotations
+
+	for _, shot := range result {
+		start, _ := ptypes.Duration(shot.StartTimeOffset)
+		end, _ := ptypes.Duration(shot.EndTimeOffset)
+
+		fmt.Fprintf(w, "Shot: %s to %s\n", start, end)
+	}
+
+	return nil
+}
+
+func TextDetectionGCS(w io.Writer, gcsURI string) error {
+	ctx := context.Background()
+
+	// Creates a client.
+	client, err := video.NewClient(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	op, err := client.AnnotateVideo(ctx, &videopb.AnnotateVideoRequest{
+		InputUri: gcsURI,
+		Features: []videopb.Feature{
+			videopb.Feature_TEXT_DETECTION,
+		},
+	})
+	if err != nil {
+		log.Fatalf("Failed to start annotation job: %v", err)
+	}
+
+	resp, err := op.Wait(ctx)
+	if err != nil {
+		log.Fatalf("Failed to annotate: %v", err)
+	}
+
+	// Only one video was processed, so get the first result.
+	result := resp.GetAnnotationResults()[0]
+
+	for _, annotation := range result.TextAnnotations {
+		fmt.Fprintf(w, "Text: %q\n", annotation.GetText())
+
+		// Get the first text segment.
+		segment := annotation.GetSegments()[0]
+		start, _ := ptypes.Duration(segment.GetSegment().GetStartTimeOffset())
+		end, _ := ptypes.Duration(segment.GetSegment().GetEndTimeOffset())
+		fmt.Fprintf(w, "\tSegment: %v to %v\n", start, end)
+
+		fmt.Fprintf(w, "\tConfidence: %f\n", segment.GetConfidence())
+
+		// Show the result for the first frame in this segment.
+		frame := segment.GetFrames()[0]
+		seconds := float32(frame.GetTimeOffset().GetSeconds())
+		nanos := float32(frame.GetTimeOffset().GetNanos())
+		fmt.Fprintf(w, "\tTime offset of the first frame: %fs\n", seconds+nanos/1e9)
+
+		fmt.Fprintf(w, "\tRotated bounding box vertices:\n")
+		for _, vertex := range frame.GetRotatedBoundingBox().GetVertices() {
+			fmt.Fprintf(w, "\t\tVertex x=%f, y=%f\n", vertex.GetX(), vertex.GetY())
+		}
+	}
+
+	return nil
+}
