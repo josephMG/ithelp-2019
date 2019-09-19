bbc5234ac6742854f0e636aeba1aa2946f79f79f
josephMG <joseph780116.mg@gmail.com>
2019-09-19 20:43:20 +0800

    [Day 12] Google Natural Language - 3

--- modules/natural_language/language.go
+++ modules/natural_language/language.go
@@ -43,3 +43,68 @@ func DemoCode(w io.Writer, text string) error {
 	}
 	return nil
 }
+
+func AnalyzeSentiment(gcsURI string) error {
+	ctx := context.Background()
+
+	// Creates a client.
+	client, err := language.NewClient(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+		return err
+	}
+	op, err := client.AnalyzeSentiment(ctx, &languagepb.AnalyzeSentimentRequest{
+		Document: &languagepb.Document{
+			Source: &languagepb.Document_GcsContentUri{
+				GcsContentUri: gcsURI,
+			},
+			Type: languagepb.Document_PLAIN_TEXT,
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Document Sentiment:\n")
+	fmt.Printf("\tScore: %.2f\n", op.DocumentSentiment.Score)
+	fmt.Printf("\tMagnitude: %.2f\n", op.DocumentSentiment.Magnitude)
+	fmt.Printf("Language: %q\n\n", op.Language)
+	for _, annotation := range op.Sentences {
+		text := annotation.GetText()
+		fmt.Printf("Text:\n")
+		fmt.Printf("\tContent: %s\n", text.Content)
+		fmt.Printf("\tSentiment: %q\n", annotation.Sentiment)
+	}
+
+	return nil
+}
+
+func AnalyzeSyntax(gcsURI string) error {
+	ctx := context.Background()
+
+	// Creates a client.
+	client, err := language.NewClient(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+		return err
+	}
+
+	op, err := client.AnnotateText(ctx, &languagepb.AnnotateTextRequest{
+		Document: &languagepb.Document{
+			Source: &languagepb.Document_GcsContentUri{
+				GcsContentUri: gcsURI,
+			},
+			Type: languagepb.Document_PLAIN_TEXT,
+		},
+		Features: &languagepb.AnnotateTextRequest_Features{
+			ExtractSyntax: true,
+		},
+		EncodingType: languagepb.EncodingType_UTF8,
+	})
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("%+v\n", op)
+	return nil
+}
